d49a714a4513db61fcdcb4450ae3558c244e199b
Kyle J. McKeown <mckeown_k@hotmail.com>
2025-07-23 21:06:13 -0400

    Update custom-package-script.bat

--- custom-package-script.bat
+++ custom-package-script.bat
@@ -1,7 +1,7 @@
 @cd /d "%~dp0\FASTER"
 
+git submodule update --init
 dotnet publish --configuration Release -a x64 --self-contained true /p:useapphost=true --output ..\Release_x64 ./FASTER.csproj
 
 @cd ..
-
 pause
